008014d82561fa9aa6e93afce786bcad6eb09050
E.T.Cook <e@etc.io>
2013-08-07 15:41:07 -0500

    Testing push for Simon
    Signed-off-by: E.T.Cook <e@etc.io>

--- shoutout.php
+++ shoutout.php
@@ -4,7 +4,7 @@ Plugin Name: Shout Out Section
 Author: etc.io
 Author URI: http://shoutout.etc.io
 Version: 1.0
-Description: An easy way to announce or make mention of a list of companies with logos!
+Description: An easy way to announce or make mention of a list of companies with logos! Smashing!
 Class Name: etcShoutOut
 Workswith: templates, main, header, footer, morefoot
 Cloning: true
